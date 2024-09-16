2c9f40d85303ec6ded9fd044e2052abfe0f82cd0
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2024-09-16 16:01:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RoomDetails from './components/RoomDetals.jsx';
 import UserHomePage from './pages/user/UserHomepage.js';
+import ManageAccommodations from './pages/admin/ManageAccommodations.jsx'; // Import the ManageAccommodations component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserHomePage />} />
+        {/* Set ManageAccommodations as the component for the root path */}
+        <Route path="/" element={<ManageAccommodations />} />
+        <Route path="/user-homepage" element={<UserHomePage />} />
         <Route path="/room/:id" element={<RoomDetails />} />
         {/* Add more routes here as needed */}
       </Routes>
